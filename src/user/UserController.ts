60fe663be12c2efbb6c37a99e0bb3387103c6d99
Jakob Niggel <jakobniggel@gmail.com>
2021-07-09 13:28:29 +0200

    change format

--- src/user/UserController.ts
+++ src/user/UserController.ts
@@ -5,29 +5,18 @@ export const urlPrefix = '/users';
 export const routes: any = {};
 
 routes.create = { method: 'POST', url: '' };
-export async function create(
-  ctx: Context,
-  params: s.CreateUser,
-): Promise<s.UserResponse> {
-  console.log("create user", params)
-  return {id: 1, name: "foo", "email": 'bar'};
+export async function create(ctx: Context, params: s.CreateUser): Promise<s.UserResponse> {
+  console.log('create user', params);
+  return { id: 1, name: 'foo', email: 'bar' };
 }
 
 routes.update = { method: 'POST', url: '/:id' };
-export async function update(
-  ctx: Context,
-  id: number,
-  params: s.UpdateUser,
-): Promise<s.UserResponse> {
-  
-  console.log("update user",id, params)
-  return {id: 1, name: "foo", "email": 'bar'};
+export async function update(ctx: Context, id: number, params: s.UpdateUser): Promise<s.UserResponse> {
+  console.log('update user', id, params);
+  return { id: 1, name: 'foo', email: 'bar' };
 }
 
 routes.list = { method: 'GET', url: '' };
-export async function list(
-  ctx: Context,
-): Promise<s.UserResponse[]> {
+export async function list(ctx: Context): Promise<s.UserResponse[]> {
   return [];
 }
-
